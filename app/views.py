e0a10bc4c9c47a960081f57ab3682ca299d99f21
leoseis <eboscrib2@yahoo.com>
2024-06-06 13:29:33 +0100

    first rest frammework page

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse 
 from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-
+@api_view(['GET',])
 def getRoutes(request):
-    return JsonResponse('hello', safe=False )
+    return Response('hello')
 
 def getProducts(request):
     return JsonResponse(products, safe=False )
--- back/settings.py
+++ back/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
